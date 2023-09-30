8b055f1dedd8a1392dc228bbe97e6e3204785c04
fabio <fabio@chelly.net>
2023-09-30 13:05:15 +0200

    v1.2.1 Importing only when necessary to reduce execution time

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
 from os.path import dirname, abspath, join
 from sys import path
 from math import pi, cos, sin, tan, log, log10, log2, exp, sqrt, acos, asin, atan, atan2, ceil, floor, degrees, radians, trunc, factorial, gcd, pow
-from subprocess import run
-from fractions import Fraction
-import uuid
-from time import time
-import os
-import hashlib
 import warnings
-from timeit import default_timer as timer
-from datetime import datetime, timedelta
-from re import findall, subn, sub, match, search, fullmatch
+from re import findall, subn, sub, match, fullmatch
 
 basedir = dirname(abspath(__file__))
 path.append(join(basedir, "lib"))
@@ -22,26 +14,19 @@ def factors(val): return FabCalc.main_intfactor(int(val), False)
 
 icon_path = join(basedir, "fabcalc.png")
 sym2int = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, 'A': 10, 'B': 11, 'C': 12, 'D': 13, 'E': 14, 'F': 15, 'G': 16, 'H': 17, 'I': 18, 'J': 19, 'K': 20, 'L': 21, 'M': 22, 'N': 23, 'O': 24, 'P': 25, 'Q': 26, 'R': 27, 'S': 28, 'T': 29, 'U': 30, 'V': 31, 'W': 32, 'X': 33, 'Y': 34, 'Z': 35, 'a': 36, 'b': 37, 'c': 38, 'd': 39, 'e': 40, 'f': 41, 'g': 42, 'h': 43, 'i': 44, 'j': 45, 'k': 46, 'l': 47, 'm': 48, 'n': 49, 'o': 50, 'p': 51, 'q': 52, 'r': 53, 's': 54, 't': 55, 'u': 56, 'v': 57, 'w': 58, 'x': 59, 'y': 60, 'z': 61}
-int2sym = {v: k for k, v in sym2int.items()}
-uuids = {"uuid", "ulid", "cuid", "sulid"}
-funcs = list(uuids) + ["now", "factors", "series", "expand_trig", "exp", "limit", "oo", "simplify", "integrate", "diff", "expand", "solve", "Fraction", "factor", "pi", "cos", "sin", "tan", "abs", "log", "log10", "log2", "exp", "sqrt", "acos", "asin", "atan", "atan2", "ceil", "floor", "degrees", "radians", "trunc", "round", "factorial", "gcd", "pow"]
+funcs = ["uuid", "ulid", "cuid", "sulid", "now", "factors", "series", "expand_trig", "exp", "limit", "oo", "simplify", "integrate", "diff", "expand", "solve", "Fraction", "factor", "pi", "cos", "sin", "tan", "abs", "log", "log10", "log2", "exp", "sqrt", "acos", "asin", "atan", "atan2", "ceil", "floor", "degrees", "radians", "trunc", "round", "factorial", "gcd", "pow"]
 funcs_pattern = r'\b(?:' + '|'.join(funcs) + r')\b'
-safe_functions = {fn: globals()[fn] for fn in funcs if fn in globals()}
-
-def fablog(s):
-    with open(join(basedir, "log.txt"), "a", encoding="utf-8") as f:
-        f.write(f"{s}\n")
 
 
 class FabCalc(FlowLauncher):
 
     @staticmethod
     def format_date(expr, result):
-        date_pattern = r'\d{4}-\d{2}-\d{2}|now'
-        dates_in_expr = len(findall(date_pattern, expr))
+        dates_in_expr = len(findall(r'\d{4}-\d{2}-\d{2}|now', expr))
     
         if dates_in_expr == 1:
             # Formater en tant que date
+            from datetime import datetime, timedelta
             base_datetime = datetime(1970, 1, 1)
             target_datetime = base_datetime + timedelta(seconds=result)
             res = target_datetime.strftime('%A %Y-%m-%d %H:%M:%S')
@@ -51,14 +36,14 @@ class FabCalc(FlowLauncher):
         days, remainder = divmod(int(result), 86400)
         hours, remainder = divmod(remainder, 3600)
         minutes, seconds = divmod(remainder, 60)
-        if days:
-            if not hours and not minutes and not seconds: return f"{days} days"
-            return f"{days} d {hours} h {minutes} m and {seconds} s"
-        return f"{hours} h {minutes} m {seconds} s"
+        if days and not hours and not minutes and not seconds: return f"{days} days"
+        res = {"d": days, "h": hours, "m": minutes, "s": seconds}
+        return " ".join([f"{v}{k}" for k, v in res.items() if v > 0])
 
     @staticmethod
     def replace_with_seconds(expr):
         if not expr or ("-" not in expr and "now" not in expr and ":" not in expr): return expr, 0
+        from datetime import datetime
         # Regex pour identifier la date, l'heure seule
         pattern = r'(?:(?P<year>\d{4})-(?P<month>\d{2})-(?P<day>\d{2})(?:\s(?P<hour>\d{2}):(?P<minute>\d{2})(?::(?P<second>\d{2}))?)?)|(?P<only_hour>\d{2}):(?P<only_minute>\d{2})(?::(?P<only_second>\d{2}))?'
     
@@ -91,6 +76,7 @@ class FabCalc(FlowLauncher):
         # Remplacer toutes les occurrences de dates et heures dans la chaîne par leur équivalent en secondes
         res, cnt = subn(pattern, compute_value, expr)
         if cnt > 0 or "now" in expr:
+            from time import time
             res = sub(r"\bnow\b", str(time()), res)
             res = sub(r"\b(\d+)w\b", r"+(\1*7*86400)", res)
             res = sub(r"\b(\d+)d\b", r"+(\1*86400)", res)
@@ -104,6 +90,7 @@ class FabCalc(FlowLauncher):
     def int2str(intval, base):
         if base == 10: return str(intval)
         res = []
+        int2sym = {v: k for k, v in sym2int.items()}
         while intval:
             intval, value = divmod(intval, base)
             res.append(int2sym[value])
@@ -178,6 +165,7 @@ class FabCalc(FlowLauncher):
     def copy_to_clipboard(self, text: str):
         p = text.find(" = ")
         if p > -1: text = text[p + 3:].strip()
+        from subprocess import run
         run("clip", input=text.encode('UTF-16LE'), check=True)
 
     def basecalc(self, query: str):
@@ -200,15 +188,19 @@ class FabCalc(FlowLauncher):
 
     @staticmethod
     def cuid():
+        from os import getpid, urandom
+        from time import time
         ts = FabCalc.int2str(int(time() * 1000), 36)
-        pid = FabCalc.int2str(os.getpid(), 36)
-        rand_block = hashlib.sha256(os.urandom(8)).hexdigest()[:20].upper()
+        pid = FabCalc.int2str(getpid(), 36)
+        import hashlib
+        rand_block = hashlib.sha256(urandom(8)).hexdigest()[:20].upper()
         rand_block = FabCalc.int2str(FabCalc.str2int(rand_block, 16), 36)
         return f"c{ts}{pid}{rand_block}"[:25].lower()
 
     @staticmethod
     def uuid(cmd):
         if cmd == "cuid": return FabCalc.cuid()
+        import uuid
         res = str(uuid.uuid4())
         if cmd != "uuid": res = res.replace("-", "")
         if cmd == "sulid": return FabCalc.int2str(FabCalc.str2int(res.upper(), 16), 62)
@@ -219,8 +211,9 @@ class FabCalc(FlowLauncher):
         pos = s.find(" ")
         if pos == -1: return
         algo = s[:pos]
-        if algo not in ("md5", "sha1", "sha256", "sha3_256", "sha3_512", "blake", "sha3", "sha512"): return
+        if algo not in ("md5", "sha1", "sha256", "sha3_256", "sha3_512", "blake"): return
         expr = s[pos + 1:].encode()
+        import hashlib
         if algo == "md5": return hashlib.md5(expr).hexdigest()
         if algo == "sha1": return hashlib.sha1(expr).hexdigest()
         if algo == "sha256": return hashlib.sha256(expr).hexdigest()
@@ -240,7 +233,7 @@ class FabCalc(FlowLauncher):
             # noinspection PyBroadException
             try:
                 # Special queries
-                if entry in uuids: return self.response(FabCalc.uuid(entry), f"{entry.upper()}: press Enter to copy to clipboard")
+                if entry in ("uuid", "ulid", "cuid", "sulid"): return self.response(FabCalc.uuid(entry), f"{entry.upper()}: press Enter to copy to clipboard")
                 res = FabCalc.hashes(entry)
                 if res: return self.response(res, f"{entry}: press Enter to copy to clipboard")
                 res = self.basecalc(entry)
@@ -253,27 +246,30 @@ class FabCalc(FlowLauncher):
                 if "(" in query and ")" not in query: query += ")"
 
                 # Symbolic formula
-                if "x" in query or "y" in query or "I" in query or "i" in query:
+                if "x" in query or "y" in query or "i" in query:
                     from sympy import series, expand_trig, oo, exp, limit, symbols, factor, expand, integrate, diff, solve, simplify, I, log, cos, sin, tan, acos, asin, atan, pi, sqrt
                     x, y = symbols("x y")
                     query = sub(r"(\d)\(", "\\1*(", query)
                     query = sub("(\d)(x)|(\d)(y)", "\\1*\\2", query)
                     query = sub("(x)(\d)|(y)(\d)", "\\1**\\2", query)
                     query = sub(r"(?<![a-z])i(?![a-z])", "I", query)
-                    res = str(eval(query, {"__builtins__": None, "series": series, "expand_trig": expand_trig, "oo": oo, "exp": exp, "limit": limit,  "x": x, "y": y, "I": I, "Fraction": Fraction, "factor": factor, "expand": expand, "integrate": integrate, "diff": diff, "solve": solve, "simplify": simplify, "log": log, "cos": cos, "sin": sin, "tan": tan, "acos": acos, "asin": asin, "atan": atan, "pi": pi, "sqrt": sqrt}))
+                    res = str(eval(query, {"__builtins__": None, "series": series, "expand_trig": expand_trig, "oo": oo, "exp": exp, "limit": limit,  "x": x, "y": y, "I": I, "factor": factor, "expand": expand, "integrate": integrate, "diff": diff, "solve": solve, "simplify": simplify, "log": log, "cos": cos, "sin": sin, "tan": tan, "acos": acos, "asin": asin, "atan": atan, "pi": pi, "sqrt": sqrt}))
                     return self.response(FabCalc.for_display(res), FabCalc.for_display(query))
 
                 # Algebric formula
                 query = sub("(\d+)!", "factorial(\\1)", query)
                 query = sub(r'(?<=\d),(?=\d)', ".", query)
-                raw_res = eval(query, {"__builtins__": None}, safe_functions)
-                res = self.fmtnum(raw_res)
+                safe = {fn: globals()[fn] for fn in funcs if fn in globals()}
+                raw = eval(query, {"__builtins__": None}, safe)
+                res = self.fmtnum(raw)
 
-                if datecnt: return self.response(FabCalc.format_date(entry, raw_res), entry)
+                if datecnt: return self.response(FabCalc.format_date(entry, raw), entry)
                 if "/" in query:
-                    query2, cnt = subn("(\d+)\s*/\s*(\d+)", "Fraction(\\1,\\2)", query)
-                    res2 = eval(query2, {"__builtins__": None}, safe_functions) if cnt else ""
-                    if "/" in str(res2): res = self.fmtnum(res2) + "  =  " + res
+                    from fractions import Fraction
+                    safe["Fraction"] = Fraction
+                    query, cnt = subn("(\d+)\s*/\s*(\d+)", "Fraction(\\1,\\2)", query)
+                    res2 = str(eval(query, {"__builtins__": None}, safe) if cnt else "")
+                    if "/" in res2: res = f"{res2}  =  {res}"
 
                 return self.response(res, FabCalc.for_display(entry))
 
